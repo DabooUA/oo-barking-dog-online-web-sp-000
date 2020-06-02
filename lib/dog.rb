8a5eec244e3cdf30828fd540bcbfb6379e4c4e98
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-02 18:30:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
 class Dog
 
   def name=(given_name)
-    new_given_name = given_name
+    @new_given_name = given_name
   end
 
-  def
+  def name
+    @new_given_name
+  end
+
+  def bark
+    puts "woof!"
+
+  end
 
 end
